39f6bc5b9c3d151b396882fae254721bd5702200
deepak prajapati <43651682+deepak7448@users.noreply.github.com>
2019-09-22 19:00:07 +0530

    emoji add

--- Ekam/settings.py
+++ Ekam/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'social_django',
+    'emoji',
     'hotel',
     'room',
     'booking',
--- Ekam/urls.py
+++ Ekam/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('booking.urls')),
     path('', include('room.urls')),
     path('', include('contact.urls')),
+    path('emoji/', include('emoji.urls')),
 ]
 urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
